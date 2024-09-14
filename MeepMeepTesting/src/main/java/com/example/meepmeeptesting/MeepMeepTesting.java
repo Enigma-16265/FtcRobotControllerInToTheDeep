ae15088d291f9dbeaf860e4b1ab0d21cbf9405a4
BudsterGaming <budstergaming@binaryflood.com>
2024-09-14 13:30:57 -0600

    mEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeEEEEEEEEEEEEEEEEEEEEEEEp mEEEEEEEEEEEEEEEEEEEEEEEEEeeEEEEEEEEEEEEEp

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,60 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MeepMeepTesting {
+    Vector2d testVector = new Vector2d(2,2);
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(60, 0, Math.toRadians(90)))
+                                .forward(60)
+                                .turn(Math.toRadians(90))
+                                .forward(120)
+                                .strafeLeft(120)
+                                .turn(Math.toRadians(180))
+                                .forward(120)
+                                .turn(Math.toRadians(90))
+                                .forward(20)
+                                .forward(20)
+                                .build()
+                );
+
+
+        Image img = null;
+        try { img = ImageIO.read(new File("C:\\Users\\Manza\\Downloads\\field.png")); }
+        catch (IOException e) {}
+
+        meepMeep.setBackground(img)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+
+
+
+        /*
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+
+         */
+
+    }
+}
