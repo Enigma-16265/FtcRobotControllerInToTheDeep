273a056879d35cb854c146567b3b0a5992dc2305
BudsterGaming <budstergaming@binaryflood.com>
2024-09-17 19:30:18 -0600

    Auto movement done theodoreredically

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,39 +17,101 @@ public class MeepMeepTesting {
 
     Vector2d testVector = new Vector2d(2,2);
     public static void main(String[] args) {
-        //Red Left = 1
-        //Red Right = 2
-        //Blue Left = 3
-        //Blue Right = 4
-        int startPos = 2;
+        //Left = 1
+        //Right = 2
+        int startPos = 1;
+
 
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(90)))
-                                .forward(20)
-                                .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
-                                //*epic depositing happens*
-                                .lineToSplineHeading(new Pose2d(-35, -25, Math.toRadians(180)))
-                                .forward(10)
-                                //*epic picking up happens*
-                                .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
-                                //*epic depositing happens*
-                                .lineToSplineHeading(new Pose2d(-45, -25, Math.toRadians(180)))
-                                .forward(10)
-                                //*epic picking up happens*
-                                .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
-                                //*epic depositing happens*
-                                .lineToSplineHeading(new Pose2d(-55, -25, Math.toRadians(180)))
-                                .forward(10)
-                                .back(15)
-                                //*epic picking up happens*
-                                .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
-                                .build()
-                );
+
+        RoadRunnerBotEntity myBot;
+
+        Pose2d redBasketPos = new Pose2d(-60, -60, Math.toRadians(225));
+
+
+        if(startPos == 1) {
+            myBot = new DefaultBotBuilder(meepMeep)
+                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                    .followTrajectorySequence(drive ->
+                            drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(90)))
+                                    .forward(20)
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    //*epic depositing happens*
+                                    .lineToSplineHeading(new Pose2d(-35, -25, Math.toRadians(180)))
+                                    .forward(10)
+                                    //*epic picking up happens*
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    //*epic depositing happens*
+                                    .lineToSplineHeading(new Pose2d(-45, -25, Math.toRadians(180)))
+                                    .forward(10)
+                                    //*epic picking up happens*
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    //*epic depositing happens*
+                                    .lineToSplineHeading(new Pose2d(-55, -25, Math.toRadians(180)))
+                                    .forward(10)
+                                    .back(15)
+                                    //*epic picking up happens*
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    .build()
+                    );
+        }
+        if(startPos == 2) {
+            myBot = new DefaultBotBuilder(meepMeep)
+                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                    .followTrajectorySequence(drive ->
+                            drive.trajectorySequenceBuilder(new Pose2d(12, -60, Math.toRadians(90)))
+                                    .forward(20)
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    .lineToSplineHeading(new Pose2d(36, -25, Math.toRadians(0)))
+                                    .forward(10)
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    .lineToSplineHeading(new Pose2d(46, -25, Math.toRadians(0)))
+                                    .forward(10)
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    .lineToSplineHeading(new Pose2d(56, -25, Math.toRadians(0)))
+                                    .forward(10)
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    .build()
+                    );
+        }
+
+
+
+
+
+
+
+
+        else {
+            myBot = new DefaultBotBuilder(meepMeep)
+                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                    .followTrajectorySequence(drive ->
+                            drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(90)))
+                                    .forward(20)
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    //*epic depositing happens*
+                                    .lineToSplineHeading(new Pose2d(-35, -25, Math.toRadians(180)))
+                                    .forward(10)
+                                    //*epic picking up happens*
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    //*epic depositing happens*
+                                    .lineToSplineHeading(new Pose2d(-45, -25, Math.toRadians(180)))
+                                    .forward(10)
+                                    //*epic picking up happens*
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    //*epic depositing happens*
+                                    .lineToSplineHeading(new Pose2d(-55, -25, Math.toRadians(180)))
+                                    .forward(10)
+                                    .back(15)
+                                    //*epic picking up happens*
+                                    .lineToSplineHeading(new Pose2d(-60, -60, Math.toRadians(225)))
+                                    .build()
+                    );
+        }
 
 
         Image img = null;
