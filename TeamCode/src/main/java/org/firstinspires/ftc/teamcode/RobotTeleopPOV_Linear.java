9db8579422cf2641765db0c92323f54363aa8d74
Gabriel <Gabriel@example.com>
2024-09-21 13:45:21 -0600

    Version 4 drive code (arm + claw)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -29,6 +29,8 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -57,20 +59,91 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
     public DcMotor leftBackDrive = null;
     public DcMotor rightFrontDrive = null;
     public DcMotor rightBackDrive = null;
-   // public DcMotor  rightDrive  = null;
-   // public DcMotor  leftArm     = null;
-    public Servo    leftClaw    = null;
-    public Servo    rightClaw   = null;
+    public DcMotor leftHang = null;
+    public DcMotor rightHang = null;
+
 
     double clawOffset = 0;
 
-    public static final double MID_SERVO   =  0.5 ;
-    public static final double CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
 
-    @Override
-    public void runOpMode() {
+    public static final double CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
+    public static final double TRIGGER_THRESHOLD = 0.5;
+    public static final double LEFT_FINGER_GRIP = 0.64;
+    public static final double RIGHT_FINGER_GRIP = 0.331;
+    public static final double LEFT_FINGER_INTAKE = 1;
+    public static final double RIGHT_FINGER_INTAKE = 0;
+
+    // rev blinkin driver
+    RevBlinkinLedDriver leftLEDSBlinkin;
+    RevBlinkinLedDriver rightLEDSBlinkin;
+
+
+    // sensors
+    private RevTouchSensor leftUpper;
+    private RevTouchSensor rightUpper;
+    private RevTouchSensor leftLower;
+    private RevTouchSensor rightLower;
+
+    // servos
+
+    private Servo leftFinger;
+    private Servo rightFinger;
+
+    private enum scoreState {
+        IDLE,
+        MOVING_LIFT,
+        MOVING_SHOULDER,
+        MOVING_WRIST,
+        MOVING_ELBOW,
+        COMPLETED
+    }
+    private scoreState currentScoreState = scoreState.IDLE;
+
+
+    private void hangCode() {
+        // hanging
+        leftHang  = hardwareMap.get(DcMotor.class, "leftHang");
+        rightHang  = hardwareMap.get(DcMotor.class, "rightHang");
+
+        leftLEDSBlinkin = hardwareMap.get(RevBlinkinLedDriver.class, "leftLEDS"); // Adjust the name as per your configuration
+        rightLEDSBlinkin = hardwareMap.get(RevBlinkinLedDriver.class, "rightLEDS"); // Adjust the name as per your configuration
+
+        leftUpper = hardwareMap.get(RevTouchSensor.class, "leftUpper");
+        rightUpper = hardwareMap.get(RevTouchSensor.class, "rightUpper");
+        leftLower = hardwareMap.get(RevTouchSensor.class, "leftLower");
+        rightLower = hardwareMap.get(RevTouchSensor.class, "rightLower");
+
+        if (gamepad1.right_trigger > TRIGGER_THRESHOLD){
+           leftLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+           rightLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+            if (!leftUpper.isPressed()) {
+                leftHang.setDirection(DcMotor.Direction.FORWARD);
+                leftHang.setPower(.9);
+            }
+            if (!rightUpper.isPressed()) {
+                rightHang.setDirection(DcMotor.Direction.FORWARD);
+                rightHang.setPower(.9);
+            }
+        } else if (gamepad1.left_trigger > TRIGGER_THRESHOLD){
+           leftLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+           rightLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+            if (!leftLower.isPressed()) {
+                leftHang.setDirection(DcMotor.Direction.REVERSE);
+                leftHang.setPower(.9);
+            }
+            if (!rightLower.isPressed()) {
+                rightHang.setDirection(DcMotor.Direction.REVERSE);
+                rightHang.setPower(.9);
+            }
+        } else {
+            leftHang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            leftHang.setPower(0);
+            rightHang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            rightHang.setPower(0);
+        }
+    }
+
+    private void driveCode() {
         double left;
         double right;
         double drive;
@@ -80,12 +153,10 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         double leftBackPower;
         double rightBackPower;
 
-        // Define and Initialize Motors
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFront");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBack");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
-        //leftArm    = hardwareMap.get(DcMotor.class, "left_arm");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
@@ -95,15 +166,68 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
-        // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drive = -gamepad1.left_stick_y;
+        turn = gamepad1.right_stick_x;
+        strafe = gamepad1.left_stick_x;
 
-        // Define and initialize ALL installed servos.
-        //leftClaw  = hardwareMap.get(Servo.class, "left_hand");
-        //rightClaw = hardwareMap.get(Servo.class, "right_hand");
-       // leftClaw.setPosition(MID_SERVO);
-       // rightClaw.setPosition(MID_SERVO);
+        left = drive + turn + strafe;
+        leftBackPower = drive + turn - strafe;
+        right = drive - turn - strafe;
+        rightBackPower = drive + turn + strafe;
+
+        // Normalize the values so neither exceed +/- 1.0
+        max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1.0)
+        {
+            left /= max;
+            right /= max;
+        }
+
+        leftFrontDrive.setPower(left);
+        leftBackDrive.setPower(leftBackPower);
+        rightFrontDrive.setPower(right);
+        rightBackDrive.setPower(rightBackPower);
+
+        if (gamepad1.right_bumper)
+            clawOffset += CLAW_SPEED;
+        else if (gamepad1.left_bumper)
+            clawOffset -= CLAW_SPEED;
+
+        clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+
+        telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+        telemetry.addData("left",  "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+        telemetry.update();
+
+    }
+
+    private void grapdropFunction() {
+
+        leftLEDSBlinkin = hardwareMap.get(RevBlinkinLedDriver.class, "leftLEDS"); // Adjust the name as per your configuration
+        rightLEDSBlinkin = hardwareMap.get(RevBlinkinLedDriver.class, "rightLEDS"); // Adjust the name as per your configuration
+
+        if (gamepad1.right_bumper && gamepad1.dpad_up && (currentScoreState == scoreState.IDLE)) {
+            leftFinger.setPosition(LEFT_FINGER_GRIP);
+            rightFinger.setPosition(RIGHT_FINGER_GRIP);
+            leftLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_MEDIUM );
+            rightLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_MEDIUM );;
+        } else if (gamepad1.right_bumper && gamepad1.dpad_left && (currentScoreState == scoreState.IDLE)) {
+            leftFinger.setPosition(LEFT_FINGER_GRIP);
+            leftLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_MEDIUM );
+        } else if (gamepad1.right_bumper && gamepad1.dpad_right && (currentScoreState == scoreState.IDLE)) {
+            rightFinger.setPosition(RIGHT_FINGER_GRIP);
+            rightLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_MEDIUM );
+        } else if (gamepad1.right_bumper && gamepad1.dpad_down && (currentScoreState == scoreState.IDLE)) {
+            leftFinger.setPosition(LEFT_FINGER_INTAKE);
+            rightFinger.setPosition(RIGHT_FINGER_INTAKE);
+            leftLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+            rightLEDSBlinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+        }
+    }
+
+    @Override
+    public void runOpMode() {
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData(">", "Robot Ready.  Press START.");    //
@@ -112,66 +236,12 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         // Wait for the game to start (driver presses START)
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
-            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
-            // This way it's also easy to just drive straight, or just turn.
-
-            drive = -gamepad1.left_stick_y;
-            turn = gamepad1.right_stick_x;
-            strafe = gamepad1.left_stick_x;
-
-
-            // Combine drive and turn for blended motion.
-            left  = drive + turn + strafe;
-            leftBackPower = drive + turn - strafe;
-            right = drive - turn - strafe;
-            rightBackPower = drive + turn + strafe;
-
-            // Normalize the values so neither exceed +/- 1.0
-            max = Math.max(Math.abs(left), Math.abs(right));
-            if (max > 1.0)
-            {
-                left /= max;
-                right /= max;
-            }
-
-            // Output the safe vales to the motor drives.
-            leftFrontDrive.setPower(left);
-            leftBackDrive.setPower(leftBackPower);
-            rightFrontDrive.setPower(right);
-            rightBackDrive.setPower(rightBackPower);
-
-
-            // Use gamepad left & right Bumpers to open and close the claw
-            if (gamepad1.right_bumper)
-                clawOffset += CLAW_SPEED;
-            else if (gamepad1.left_bumper)
-                clawOffset -= CLAW_SPEED;
-
-            // Move both servos to new position.  Assume servos are mirror image of each other.
-            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
-          //  leftClaw.setPosition(MID_SERVO + clawOffset);
-          //  rightClaw.setPosition(MID_SERVO - clawOffset);
-
-            // Use gamepad buttons to move arm up (Y) and down (A)
-            //if (gamepad1.y)
-                //leftArm.setPower(ARM_UP_POWER);
-           // else if (gamepad1.a)
-                //leftArm.setPower(ARM_DOWN_POWER);
-           // else
-                //leftArm.setPower(0.0);
-
-            // Send telemetry message to signify robot running;
-            telemetry.addData("claw",  "Offset = %.2f", clawOffset);
-            telemetry.addData("left",  "%.2f", left);
-            telemetry.addData("right", "%.2f", right);
-            telemetry.update();
-
-            // Pace this loop so jaw action is reasonable speed.
-            sleep(50);
+        while(opModeIsActive()){
+            driveCode();
+            hangCode();
+            grapdropFunction();
         }
+
+        sleep(50);
     }
 }
