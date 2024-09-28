2a1dcacc7ca84f66bbc0bf7a5328a9d83f25fc5f
NinjaPenguin <noiwont@no.com>
2024-09-28 11:01:52 -0600

    Int code is almost done I just need the names fro the real robot not the test one

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BrokenVessel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BrokenVessel.java
@@ -78,7 +78,7 @@ public void ServoPos() {
 
         while(opModeIsActive()) {
             //telemetry.addData("Selected", which.toString());
-            //telemetry.addLine("Y = Shoulder - X = Hopper - B = Wrist A = Lift");
+            //telemetry.addLine("Y = Shoulder - X = Hopper - B = Wrist A = Lift");w
             telemetry.update();
             sleep(100);
         }
