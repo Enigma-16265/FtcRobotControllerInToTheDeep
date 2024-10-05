636af09a361197b33a94de8b038dca1351d07c91
Manza <manza@example.com>
2024-10-05 12:12:20 -0600

    Refactor drive and arm code into standalone classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_LinearWithArm.java
@@ -0,0 +1,79 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/*
+ * This code calls LorelaisDriveCode.java and ShoulderElbowClawCode.java.
+ * It runs the wheel drive code and the arm movement code, so the robot can
+ * move around and stuff
+ */
+
+@TeleOp(name="Robot: Teleop POV", group="Robot")
+//@Disabled
+public class RobotTeleopPOV_LinearWithArm extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        // Define and initialize wheels and declare wheelCode
+        LorelaisDriveCode wheelCode = new LorelaisDriveCode(hardwareMap, gamepad1);
+        // Define and initialize ALL installed servos and declare armCode
+        ShoulderElbowClawCode armCode = new ShoulderElbowClawCode(hardwareMap, gamepad1);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(">", "Robot Ready.  Press START.");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // run various control code
+            wheelCode.runWheels();
+            armCode.runClaw();
+            armCode.runShoulder();
+            armCode.runElbow();
+            armCode.runWrist();
+
+
+            // Send telemetry message to signify robot running
+            // should i have the control code return some info for this? Does
+            // anyone even care?
+            telemetry.update();
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
