88685d2b69cd7357875f9397b8ba101c0f565468
cyborgWyrm <manza@example.com>
2024-11-05 16:34:52 -0700

    write sammys drive code and drive code abstract

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SammysDriveCode.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class SammysDriveCode extends DriveCodeAbstract {
+
+
+    public SammysDriveCode(HardwareMap hardwareMap, Gamepad gamepad1) {
+        super(hardwareMap, gamepad1);
+    }
+
+    public void runWheels() {
+        double drive = -gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+        double strafe = gamepad1.left_stick_x;
+
+        strafe = Math.round(strafe);
+        drive = Math.round(drive);
+
+        double left = drive + turn + strafe;
+        double leftBackPower = drive + turn - strafe;
+        double right = drive - turn - strafe;
+        double rightBackPower = drive + turn + strafe;
+
+        // Normalize the values so neither exceed +/- 1.0
+        double max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1.0)
+        {
+            left /= max;
+            right /= max;
+        }
+
+        // Output the safe vales to the motor drives.
+        leftFrontDrive.setPower(left * speed);
+        leftBackDrive.setPower(leftBackPower * speed);
+        rightFrontDrive.setPower(right * speed);
+        rightBackDrive.setPower(rightBackPower * speed);
+    }
+}
