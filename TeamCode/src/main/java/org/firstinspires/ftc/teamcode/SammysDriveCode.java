e9616d77a9a5037634b80a02639679354d9f18b2
cyborgWyrm <manza@example.com>
2024-11-05 17:07:31 -0700

    write sammys drive code and drive code abstract

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SammysDriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SammysDriveCode.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class SammysDriveCode extends DriveCodeAbstract {
 
+    private boolean wasStrafing = false;
+    private double whenStrafeStarted;
 
     public SammysDriveCode(HardwareMap hardwareMap, Gamepad gamepad1) {
         super(hardwareMap, gamepad1);
@@ -29,6 +31,10 @@ public class SammysDriveCode extends DriveCodeAbstract {
         {
             left /= max;
             right /= max;
+        }
+
+        if (!wasStrafing && strafe != 0) {
+            whenStrafeStarted = System.currentTimeMillis();
         }
 
         // Output the safe vales to the motor drives.
