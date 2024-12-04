37ea667d42183ad8b9496ed884bc2b4b8b2bd548
NinjaPenguin <noiwont@no.com>
2024-12-03 19:11:58 -0700

    starting to make mantas able to do laps.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mantas/ControlClassFiles/RandomMovementControlsMantas.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mantas/ControlClassFiles/RandomMovementControlsMantas.java
@@ -105,10 +105,10 @@ public class RandomMovementControlsMantas {
                 currently_doing_a_lap = false;
                 cycle_number_lap = 14;
 
-                leftFrontWheel.setPower(speed);
-                leftBackWheel.setPower(speed);
-                rightFrontWheel.setPower(-speed);
-                rightBackWheel.setPower(-speed);
+                leftFrontWheel.setPower(0.25);
+                leftBackWheel.setPower(0.25);
+                rightFrontWheel.setPower(-0.25);
+                rightBackWheel.setPower(-0.25);
             }
         }
     }
