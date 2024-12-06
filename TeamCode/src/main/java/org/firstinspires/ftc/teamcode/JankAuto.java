7850356968a6aaa0f70ad94d4185c74cdb7340fb
BudsterGaming <budstergaming@binaryflood.com>
2024-12-05 19:27:07 -0700

    why didnt it go what

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JankAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JankAuto.java
@@ -27,58 +27,99 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This OpMode illustrates the concept of driving a path based on time.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backward for 1 Second
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
-@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
-public class RobotAutoDriveByTime_Linear extends LinearOpMode {
+
+@Autonomous(name="JankAuto", group="Robot")
+public class JankAuto extends LinearOpMode {
 
     /* Declare OpMode members. */
     private DcMotor         leftDrive   = null;
     private DcMotor         rightDrive  = null;
+    private DcMotor rightFront;
+    private DcMotor leftFront;
+    private DcMotor rightBack;
+    private DcMotor leftBack;
 
     private ElapsedTime     runtime = new ElapsedTime();
 
 
     static final double     FORWARD_SPEED = 0.6;
     static final double     TURN_SPEED    = 0.5;
+    private void left() {
+        leftFront.setPower(-0.5);
+        rightBack.setPower(-0.5);
+        leftBack.setPower(0.5);
+        rightFront.setPower(0.5);
+    }
+    private void right() {
+        leftFront.setPower(0.5);
+        rightBack.setPower(0.5);
+        leftBack.setPower(-0.5);
+        rightFront.setPower(-0.5);
+    }
+    private void forward() {
+        leftFront.setPower(0.5);
+        rightBack.setPower(0.5);
+        leftBack.setPower(0.5);
+        rightFront.setPower(0.5);
+    }
+    private void backward() {
+        leftFront.setPower(-0.5);
+        rightBack.setPower(-0.5);
+        leftBack.setPower(-0.5);
+        rightFront.setPower(-0.5);
+    }
+    private void rotateLeft() {
+        leftFront.setPower(0.5);
+        rightBack.setPower(-0.5);
+        leftBack.setPower(0.5);
+        rightFront.setPower(-0.5);
+    }
+    private void rotateRight() {
+        leftFront.setPower(-0.5);
+        rightBack.setPower(0.5);
+        leftBack.setPower(-0.5);
+        rightFront.setPower(0.5);
+    }
+    private void brake() {
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+    }
+    private void zzz(long howlong) {
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        sleep(howlong);
+    }
 
     @Override
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        leftDrive  = hardwareMap.get(DcMotor.class, "rightFront");
-        rightDrive = hardwareMap.get(DcMotor.class, "leftFront");
+        //leftDrive  = hardwareMap.get(DcMotor.class, "rightFront");
+        //rightDrive = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
@@ -90,14 +131,27 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         // Step through each leg of the path, ensuring that the OpMode has not been stopped along the way.
 
         // Step 1:  Drive forward for 3 seconds
-        leftDrive.setPower(FORWARD_SPEED);
-        rightDrive.setPower(FORWARD_SPEED);
+        //leftFront.setPower(FORWARD_SPEED);
+        //rightFront.setPower(FORWARD_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
 
+        telemetry.addData("Status", "Running.");
+        telemetry.update();
+        sleep(1000);
+        forward();
+        sleep(150);
+        left();
+        sleep(2500);
+        brake();
+        sleep(1000);
+        rotateLeft();
+        sleep(500);
+        brake();
+        /*
         // Step 2:  Spin right for 1.3 seconds
         leftDrive.setPower(TURN_SPEED);
         rightDrive.setPower(-TURN_SPEED);
@@ -123,5 +177,7 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         telemetry.addData("Path", "Complete");
         telemetry.update();
         sleep(1000);
+        */
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAuto.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@Autonomous
+
+@Autonomous(name="TimedAuto", group="Robot")
 public class TimedAuto extends LinearOpMode {
     private final DcMotor rightFront;
     private final DcMotor leftFront;
@@ -16,7 +19,7 @@ public class TimedAuto extends LinearOpMode {
 
     /** @noinspection FieldMayBeFinal, FieldCanBeLocal */ //not objects
     // constructor
-    public TimedAuto (HardwareMap hardwareMap, Gamepad gamePad) {
+    public TimedAuto (@NonNull HardwareMap hardwareMap, Gamepad gamePad) {
         //sets all the wheels to what they are in the hardware map
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -81,11 +84,21 @@ public class TimedAuto extends LinearOpMode {
         rightFront.setPower(0);
         sleep(howlong);
     }
-    @Override public void runOpMode() {
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
 
         waitForStart();
+        // while (opModeInInit()) {}
 
-        while (opModeIsActive()) {
+       // while (opModeIsActive()) {
+            telemetry.addData("Status", "Running.");
+            telemetry.update();
+            sleep(1000);
+            forward();
+            sleep(150);
             left();
             sleep(1000);
             brake();
@@ -95,7 +108,12 @@ public class TimedAuto extends LinearOpMode {
             backward();
             sleep(100);
             brake();
-        }
+            //do the thing
+        //}
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
     }
 
 }
