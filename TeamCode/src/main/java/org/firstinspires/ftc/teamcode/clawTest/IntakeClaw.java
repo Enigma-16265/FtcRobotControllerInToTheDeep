13b389aa0796758138986cda350dc82a7275ad51
Knight314159 <SAGR@adisteam.org>
2024-12-26 14:49:31 -0700

    Print out box corners so I can check if my code will work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawTest/IntakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawTest/IntakeClaw.java
@@ -2,18 +2,26 @@ package org.firstinspires.ftc.teamcode.clawTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.Mantas.ControlClassFiles.RosesMcMuffinClass;
 import org.firstinspires.ftc.teamcode.hypotheticals.ColorLocator_Loralei;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
 @Autonomous(name = "Concept: Test", group = "Concept")
-public class IntakeClaw extends ColorLocator_Loralei {
+public class IntakeClaw extends ColorLocator_LoraleiButEditedToWorkForIntakeClaw {
+    RosesMcMuffinClass movement = new RosesMcMuffinClass(hardwareMap, gamepad1);
 
     @Override
     public void runOpMode() {
         super.Setup();
-        while (opModeIsActive() || opModeInInit()){
-            super.colorFinder();
-            if (super.angle >= 1) {
-            telemetry.addLine("Angle >= 0");
-            }
-        }
+        //Take the arcos to find the angle
+        double ydist = myBoxCorners[0].y - myBoxCorners[1].y;
+        double xdist = myBoxCorners[0].x - myBoxCorners[1].x;
+        double disthypot = Math.pow(xdist,2) + Math.pow(ydist,2);
+        double angle = Math.acos((ydist)/(Math.sqrt(disthypot)));
+        //whatever servo you're using .setPosition(angle + expirimental modifier)
     }
 }
