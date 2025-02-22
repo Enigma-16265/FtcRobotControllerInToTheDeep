215a5fd429efd168be16ec6cd1531954aa180bc8
NinjaPenguin <noiwont@no.com>
2025-02-22 16:54:42 -0700

    Personal Drive Code File

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mantas/opmodes/OliversDriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mantas/opmodes/OliversDriveCode.java
@@ -29,35 +29,58 @@
 
 package org.firstinspires.ftc.teamcode.Mantas.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
-
-/*
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
+import javax.script.*;
 
 @TeleOp(name="Olivers Drive Code") //, group="Linear OpMode")
-@Disabled
 public class OliversDriveCode extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor leftFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+
+    // Used to reverse the controls if necessary
+    private int driveReverse = 1;
+    private int turnReverse = 1;
+    private int rightReverse = 1;
+    private int leftReverse = 1;
+
+    // Variables to prevent control loops
+    private boolean xPressed = false;
+    private boolean yPressed = false;
+    private boolean aPressed = false;
+    private boolean bPressed = false;
+    private boolean rbPressed = false; //Right Bumper
+    private boolean rtPressed = false; //Right Trigger
+    private boolean lbPressed = false; //Left Bumper
+    private boolean ltPressed = false; //Left Trigger
+    private boolean rsPressed = false; //Right Stick
+    private boolean lsPressed = false; //Left Stick
+
+
+    private enum Buttons {
+        aButton,
+        bButton,
+        xButton,
+        yButton,
+        rightBumper,
+        rightTrigger,
+        leftBumper,
+        leftTrigger,
+        rightStick,
+        leftStick,
+        dpadUp,
+        dpadDown,
+        dpadRight,
+        dpadLeft,
+    }
 
     @Override
     public void runOpMode() {
@@ -65,16 +88,10 @@ public class OliversDriveCode extends LinearOpMode {
         telemetry.update();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
 
         // Wait for the game to start (driver presses START)
         waitForStart();
@@ -84,32 +101,61 @@ public class OliversDriveCode extends LinearOpMode {
         while (opModeIsActive()) {
 
             // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+            double drivePower = gamepad1.left_stick_x;
+            double turnPower = gamepad1.left_stick_y;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+            // Check if X/Y is pressed on controller
+            // If so reverse drive and turn functions
+            if (gamepad1.x && !xPressed) {
+                xPressed = true;
+                driveReverse *= -1;
+            } else if (!gamepad1.x) {
+                xPressed = false;
+            }
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            if (gamepad1.y && !yPressed) {
+                yPressed = true;
+                turnReverse *= -1;
+            } else if (!gamepad1.y) {
+                yPressed = false;
+            }
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+
+
+            // Calculate power from variables
+            double rightPower = Range.clip(drivePower + (turnPower * turnReverse), -1.0, 1.0) * driveReverse;
+            double leftPower = Range.clip(drivePower - (turnPower * turnReverse), -1.0, 1.0) * driveReverse;
 
             // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+            rightFront.setPower(rightPower);
+            rightBack.setPower(rightPower);
+            leftFront.setPower(leftPower);
+            leftBack.setPower(leftPower);
 
-            // Show the elapsed game time and wheel power.
+            // Stuff to print to the console (clears every loop)
+            telemetry.addData("Version", "1.1");
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Reverses", "Drive: [%d], Turn: [%d]", driveReverse, turnReverse);
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
         }
+    }
+
+    private static class bind {
+         public class add<T> {
+             Buttons button;
+             T variable;
+             T modifier;
+
+             public add(Buttons Button, T Variable, T Modifier) {
+                 this.button = Button;
+                 this.variable = Variable;
+                 this.modifier = Modifier;
+             }
+
+             public void check() {
+
+             }
+         }
     }
 }
